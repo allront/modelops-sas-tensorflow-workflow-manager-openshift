512fc5fe9025823755bbf5d19c9e63e5056ab8f4
ivnard <ivan.nardini@sas.com>
2020-09-30 17:59:27 +0200

    Api caller model settings

--- src/api_caller_model.py
+++ src/api_caller_model.py
@@ -8,4 +8,7 @@ import logging
 
 # Create a logging for each request
 
-
+# Request example 
+# curl -v -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://championmodel-deploymodel1.10.249.20.186.nip.io/v1/models/champion_model:predict
+# Notice server domain may change.
+# Notice endpoint /champion_model:predict may change as well
