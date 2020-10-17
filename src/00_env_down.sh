c7098fc5f8904d3184a53e0b4b782a423b792b2f
ivnard <ivan.nardini@sas.com>
2020-10-17 11:03:30 +0200

    env down script

--- src/00_env_down.sh
+++ src/00_env_down.sh
@@ -1,1 +1,21 @@
-oc delete all --all
+#!/usr/bin/env bash
+# O_env_down.sh
+# O_env_down.sh is a script for delete all resources and stop the minishift
+#
+# Variables:
+# PROJECTNAME=${1:sasmlopshmeq} path of the folder with minishift
+#
+# Steps:
+# 1 - Delete project
+# 2 - Stop minishift
+
+#Variables
+PROJECTNAME=${1:sasmlopshmeq}
+
+# 1 - Delete project
+echo "$(date '+%x %r') INFO Deleting OKD ${PROJECTNAME} project..."
+oc delete project ${PROJECTNAME}
+
+# 2 - Stop minishift
+echo "$(date '+%x %r') INFO Stop minishift..."
+minishift stop
