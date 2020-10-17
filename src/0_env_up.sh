5b8cec3a3d55a8592bbe89be09f9f53561cb894f
ivnard <ivan.nardini@sas.com>
2020-10-17 09:56:07 +0200

    modify env up

--- src/0_env_up.sh
+++ src/0_env_up.sh
@@ -3,33 +3,26 @@
 # O_env_up.sh is a script for setting all demo variables and spin up the OKD environment from control node
 #
 # Variables:
-# PROJECT_DIR=${1} path of the project folder
-# MINISHIFTPATH=${2:-/home/ec2-user/minishift-1.34.2-linux-amd64/} path of the folder with minishift
-# REMOTEIP=${3:-10.249.20.186} the ip of remote OKD
-# USEROKD=${4:-root}
+# MINISHIFTPATH=${1:-/home/ec2-user/minishift-1.34.2-linux-amd64/} path of the folder with minishift
+# REMOTEIP=${2:-10.249.20.186} the ip of remote OKD
+# USEROKD=${3:-root}
 #
 # Steps:
-# 1 - Export the path of the project folder
-# 2 - Export minishift path
-# 3 - Start minishift and set oc cli
+# 1 - Export minishift path
+# 2 - Start minishift and set oc cli
 
 #Variables
-PROJECT_DIR=${1}
 MINISHIFTPATH=${1:-/home/ec2-user/minishift-1.34.2-linux-amd64/}
 REMOTEIP=${2:-10.249.20.186}
 USEROKD=${3:-root}
 
-# 1 - Export the path of the project folder
-echo "$(date '+%x %r') INFO Export the path of the project folder..."
-export PROJECT_DIR=/opt/demos/modelops
-
-# 2 - Export minishift path
+# 1 - Export minishift path
 echo "$(date '+%x %r') INFO Export Minishift path and check status..."
 export PATH=$PATH:${MINISHIFTPATH}
 STATUS=$(minishift status | head -n 3 | tail -n 1 | cut -d \   -f 3)
 echo "$(date '+%x %r') INFO The remote Openshift enviroment is ${STATUS}!"
 
-# 3 - Start minishift
+# 2 - Start minishift
 if [ ${STATUS} != "stopped" ]; then
 
   echo "$(date '+%x %r') INFO Starting the remote Openshift..."
@@ -47,6 +40,10 @@ if [ ${STATUS} != "stopped" ]; then
   echo "2. Export path as the command suggests"
   echo ""
   echo "3. Run the following command to get info about user and project"
+  echo ""
+  echo "4. Remember to export the PROJECT_DIR path"
+  echo ""
+  echo "export PROJECT_DIR=</path/of/project>"
   echo ""
   echo "---------------------------------------------------------------"
 
