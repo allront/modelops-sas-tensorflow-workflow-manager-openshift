ca87d44de720ccaf154160f154cd842d61ab1daa
ivnard <ivan.nardini@sas.com>
2020-10-18 07:52:20 +0200

    export variables notes

--- src/0_env_up.sh
+++ src/0_env_up.sh
@@ -8,7 +8,7 @@
 # USEROKD=${3:-root}
 #
 # Steps:
-# 1 - Export minishift path
+# 1 - Check cluster status
 # 2 - Start minishift and set oc cli
 
 #Variables
@@ -16,8 +16,8 @@ MINISHIFTPATH=${1:-/home/ec2-user/minishift-1.34.2-linux-amd64/}
 REMOTEIP=${2:-10.249.20.186}
 USEROKD=${3:-root}
 
-# 1 - Export minishift path
-echo "$(date '+%x %r') INFO Export Minishift path and check status..."
+# 1 - Check cluster status
+echo "$(date '+%x %r') INFO Check cluster status..."
 export PATH=$PATH:${MINISHIFTPATH}
 STATUS=$(minishift status | head -n 3 | tail -n 1 | cut -d \   -f 3)
 echo "$(date '+%x %r') INFO The remote Openshift enviroment is ${STATUS}!"
@@ -43,9 +43,10 @@ if [ ${STATUS} != "stopped" ]; then
   echo ""
   echo "oc status"
   echo ""
-  echo "4. Remember to export the PROJECT_DIR path"
+  echo "4. Remember to export the PROJECT_DIR and mnishift paths"
   echo ""
   echo "export PROJECT_DIR=</path/of/project>"
+  echo "export export PATH=$PATH:/home/ec2-user/minishift-1.34.2-linux-amd64/"
   echo ""
   echo "---------------------------------------------------------------"
 
